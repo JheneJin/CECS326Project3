0299a7579ff182f6189232650ddfe6972a719aaa
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-10-27 11:01:00 -0700

    Update westVill.java

--- westVill.java
+++ westVill.java
@@ -1,8 +1,10 @@
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class westVill implements Runnable
 {
    int pplNum;
+   public Semaphore semaphore;
    public Thread thread;
    String [] op = {" drinking water ", " talking to the boys ", " working out ", " asking for directions", " looking for baddies"};
    Random randOp = new Random();
@@ -13,26 +15,38 @@ public class westVill implements Runnable
       return rand;
    }
   
-   public westVill(int pplIndex){
+   public westVill(int pplIndex, Semaphore semaphore){
       pplNum = pplIndex;
+      this.semaphore = semaphore;
       //this implements the Runnable for the thread
       thread = new Thread(this);
       thread.start();
    }
 
-   public void drive(){
-      try{
-         Thread.sleep(randSecs());
-         System.out.println("West Villager " + pplNum + " is driving on the road");
-         System.out.println("West Villager " + pplNum + op[randOp.nextInt(op.length)]);
-      }
-      catch(InterruptedException e){
-         System.out.println("Error happened.");
-      }
-   }
+  public void drive() {
+    try {
+        Thread.sleep(randSecs());
+        semaphore.acquire();
+
+        while (RoadController.villFlag()) {
+            semaphore.release();
+            Thread.sleep(randSecs()); 
+            semaphore.acquire();
+        }
+
+        System.out.println("West Villager " + pplNum + " is driving on the road");
+        System.out.println("West Villager " + pplNum + op[randOp.nextInt(op.length)]);
+        Thread.sleep(randSecs());
+        System.out.println("West Villager " + pplNum + " has finished the exchange");
+        RoadController.switchTurn();
+        semaphore.release();
+    } catch (InterruptedException e) {
+        System.out.println("Error happened.");
+    }
+}
 
    public void run(){
       drive();
-      System.out.println("West Villager " + pplNum + " has finished the exchange");
    }
 }
+
