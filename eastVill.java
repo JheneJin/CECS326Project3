40f3c64536fef18060695f740d6940ea1d59121f
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-10-27 11:00:48 -0700

    Update eastVill.java

--- eastVill.java
+++ eastVill.java
@@ -1,8 +1,10 @@
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class eastVill implements Runnable
 {
    int pplNum;
+   public Semaphore semaphore;
    public Thread thread;
    String [] op = {" drinking water ", " talking to the boys ", " working out ", " asking for directions", " looking for baddies"};
    Random randOp = new Random();
@@ -12,27 +14,50 @@ public class eastVill implements Runnable
       int rand = r.nextInt(3000);
       return rand;
    }
-
-   public eastVill(int pplIndex){
+  
+   public eastVill(int pplIndex, Semaphore semaphore){
       pplNum = pplIndex;
-       //this implements the Runnable for the thread
+      this.semaphore = semaphore;
+      //this implements the Runnable for the thread
       thread = new Thread(this);
       thread.start();
    }
 
-   public void drive(){
-      try{
-         Thread.sleep(randSecs());
-         System.out.println("East Villager " + pplNum + " is driving on the road");
-         System.out.println("East Villager " + pplNum + op[randOp.nextInt(op.length)]);
-      }
-      catch(InterruptedException e){
-         System.out.println("Error happened.");
-      }
-   }
+   public void drive() {
+    try {
+        //for the first essential villager
+        //makes all the villagers sleep so not all the villagers will try to get the semapgore
+        Thread.sleep(randSecs());
+        // the villager acqiiores tjhe semaphore
+        semaphore.acquire();
+        
+        //while the villFlag is not true
+        while (!RoadController.villFlag()) {
+            //if its not the right villagers turn, it will rellease the semaphore
+            semaphore.release();
+            //makes it sleep, so another villager who has the permission to acquire
+            Thread.sleep(randSecs()); 
+            semaphore.acquire();
+        }
+
+        //displays the east villlager driving and completing the task
+        System.out.println("East Villager " + pplNum + " is driving on the road");
+        System.out.println("East Villager " + pplNum + op[randOp.nextInt(op.length)]);
+        //tells thread or villager to go sleep
+        Thread.sleep(randSecs());
+        System.out.println("East Villager " + pplNum + " has finished the exchange");
+        //changes the boolean here so the counterpart villager can use the semaphore
+        RoadController.switchTurn();
+        //releases the current village semaphore right afgter
+        semaphore.release();
+        
+    } catch (InterruptedException e) {
+        System.out.println("Error happened.");
+    }
+}
 
    public void run(){
       drive();
-      System.out.println("East Villager " + pplNum + " has finished the exchange");
    }
 }
+
